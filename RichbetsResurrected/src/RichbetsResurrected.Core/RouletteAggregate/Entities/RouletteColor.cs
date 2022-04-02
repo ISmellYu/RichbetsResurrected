f4a4fc8c948e0abbaa9cd3b38b8a37aa2a017cfe
ISmellYu <lukaszszostak10@gmail.com>
2022-04-02 21:00:39 +0200

    Prepared to implement RouletteService.cs

---
+++ RichbetsResurrected/src/RichbetsResurrected.Core/RouletteAggregate/Entities/RouletteColor.cs
@@ -0,0 +1,8 @@
+﻿namespace RichbetsResurrected.Core.RouletteAggregate.Entities;
+
+public enum RouletteColor
+{
+    Red,
+    Black,
+    Green
+}
---
+++ RichbetsResurrected/src/RichbetsResurrected.Infrastructure/Games/Roulette/RouletteService.cs
@@ -0,0 +1,15 @@
+﻿using RichbetsResurrected.Core.RouletteAggregate.Entities;
+
+namespace RichbetsResurrected.Infrastructure.Games.Roulette;
+
+public class RouletteService
+{
+    private List<RoulettePlayer> Players { get; set; } = new();
+    private bool IsRunning { get; set; } = false;
+    
+
+    public RouletteService()
+    {
+    }
+    
+}
