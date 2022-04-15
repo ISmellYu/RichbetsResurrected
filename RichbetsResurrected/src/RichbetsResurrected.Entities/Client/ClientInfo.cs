31cb8fdd5434957bd532b0fefcfe23e5fef28692
ISmellYu <lukaszszostak10@gmail.com>
2022-04-16 00:20:29 +0200

    Added ClientHub.cs

---
+++ RichbetsResurrected/src/RichbetsResurrected.Entities/Client/ClientInfo.cs
@@ -0,0 +1,12 @@
+﻿using RichbetsResurrected.Entities.DatabaseEntities;
+
+namespace RichbetsResurrected.Entities.Client;
+
+public class ClientInfo
+{
+    public int IdentityUserId { get; set; }
+    public string DiscordUserId { get; set; }
+    public string UserName { get; set; }
+    public string AvatarUrl { get; set; }
+    public RichbetUser RichbetUser { get; set; }
+}
