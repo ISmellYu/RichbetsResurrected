5fb593bc33f47510b0b8f40d89a27604b93c63bc
Drairan9 <drairan@protonmail.com>
2022-04-15 21:15:23 +0200

    roulette - created control bar controller

---
+++ RichbetsResurrected/src/RichbetsResurrected.Web/wwwroot/js/Games/Roulette/roulette-bar-controller.rb.js
@@ -0,0 +1,46 @@
+$('.control-bar-button').click(function () {
+
+    let inputField = document.querySelector('#coins');
+    let action = $(this).attr("action");
+
+
+    if (inputField.value == "") {
+        inputField.value = 0;
+    }
+
+
+    switch (action) {
+        case "clear":
+            inputField.value = "";
+            break;
+
+        case "1":
+            inputField.value = parseInt(inputField.value) + 1;
+            break;
+
+        case "10":
+            inputField.value = parseInt(inputField.value) + 10;
+            break;
+
+        case "100":
+            inputField.value = parseInt(inputField.value) + 100;
+            break;
+
+        case "1000":
+            inputField.value = parseInt(inputField.value) + 1000;
+            break;
+
+        case "half":
+            inputField.value = (inputField.value / 2).toFixed(0);
+            break;
+
+        case "double":
+            inputField.value = parseInt(inputField.value * 2);
+            break;
+
+        case "max":
+            //todo
+            break;
+    }
+
+});
