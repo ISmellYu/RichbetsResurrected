70dc2116552b874254783b64103fb7dd671ce5a1
Drairan9 <drairan@protonmail.com>
2022-04-16 01:59:21 +0200

    timer bug fixed

--- RichbetsResurrected/src/RichbetsResurrected.Web/wwwroot/js/Games/Roulette/roulette-controller.rb.js
+++ RichbetsResurrected/src/RichbetsResurrected.Web/wwwroot/js/Games/Roulette/roulette-controller.rb.js
@@ -207,7 +207,7 @@ conn.start().then(function () {
 
             let actualProgress = (data.timeLeft * 100) / 15; // Actual progress. 0-100.
 
-            let timeText = data.timeLeft.toFixed(1); // Split output data into one decimal place.
+            let timeText = toFixed(data.timeLeft, 1) // Split output data into one decimal place.
 
             if (actualProgress < 98) { // Prevent the timer from visual bug.
 
@@ -226,6 +226,20 @@ conn.start().then(function () {
             }
         }
     });
+
+    function toFixed(value, precision) {
+            precision = precision || 0,
+            power = Math.pow(10, precision),
+            absValue = Math.abs(Math.round(value * power)),
+            result = (value < 0 ? '-' : '') + String(Math.floor(absValue / power));
+    
+        if (precision > 0) {
+            let fraction = String(absValue % power),
+                padding = new Array(Math.max(precision - fraction.length, 0) + 1).join('0');
+            result += '.' + padding + fraction;
+        }
+        return result;
+    }
 
 
     conn.on("EndRoulette", function (history, current) {
