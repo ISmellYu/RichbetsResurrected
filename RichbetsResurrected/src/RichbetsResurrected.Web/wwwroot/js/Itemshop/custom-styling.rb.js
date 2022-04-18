868fc21583ade6f041d64cd7f61fef3ff8988d0b
Drairan9 <drairan@protonmail.com>
2022-04-18 16:17:42 +0200

    Itemshop W.I.P

---
+++ RichbetsResurrected/src/RichbetsResurrected.Web/wwwroot/js/Itemshop/custom-styling.rb.js
@@ -0,0 +1,22 @@
+const Items = [
+    {
+        "Id": "1",
+        "Name": "tiger",
+        "Description": "A tiger",
+        "ImageUrl": "url",
+        "Price": "100",
+        "AvailableQuantity": "1",
+        "IsUsable": "true",
+        "CategoryId": "Pattern",
+    },
+    {
+        "Id": "2",
+        "Name": "giraffe",
+        "Description": "A tiger",
+        "ImageUrl": "url",
+        "Price": "100",
+        "AvailableQuantity": "1",
+        "IsUsable": "true",
+        "CategoryId": "Pattern",
+    }
+];
