333ff9651aabfdd9f0a0faa466db180d116c87bd
ISmellYu <lukaszszostak10@gmail.com>
2022-04-19 19:12:31 +0200

    Added IShopService.cs

---
+++ RichbetsResurrected/src/RichbetsResurrected.Interfaces/Shop/IShopService.cs
@@ -0,0 +1,24 @@
+﻿using RichbetsResurrected.Entities.DatabaseEntities.Shop;
+
+namespace RichbetsResurrected.Interfaces.Shop;
+
+public interface IShopService
+{
+    List<Item> GetItems();
+    List<Category> GetCategories();
+    List<SubCategory> GetSubCategories();
+    List<Discount> GetDiscounts();
+    List<ActiveItem> GetActiveItems();
+    List<ConsumableItem> GetConsumableItems();
+    List<UserItem> GetUserItems();
+    List<ItemType> GetItemTypes();
+    int CalculateTotalPriceForItem(Item item);
+    ActiveItem? GetActiveItemByIds(int richbetUserId, int itemId);
+    Category? GetCategoryById(int categoryId);
+    SubCategory? GetSubCategoryById(int subCategoryId);
+    ConsumableItem? GetConsumableItemByItemId(int itemId);
+    Discount? GetDiscountByItemId(int itemId);
+    Item? GetItemById(int itemId);
+    UserItem? GetUserItemByIds(int richbetUserId, int itemId);
+    ItemType? GetItemTypeByItemId(int itemId);
+}
--- RichbetsResurrected/src/RichbetsResurrected.Services/Shop/ShopService.cs
+++ RichbetsResurrected/src/RichbetsResurrected.Services/Shop/ShopService.cs
@@ -1,9 +1,10 @@
 ﻿using RichbetsResurrected.Entities.DatabaseEntities.Shop;
 using RichbetsResurrected.Interfaces.DAL.Shop;
+using RichbetsResurrected.Interfaces.Shop;
 
 namespace RichbetsResurrected.Services.Shop;
 
-public class ShopService
+public class ShopService : IShopService
 {
     private readonly IShopRepository _shopRepository;
     public ShopService(IShopRepository shopRepository)
@@ -20,14 +21,6 @@ public class ShopService
     {
         return _shopRepository.GetCategories();
     }
-
-    public int CalculateTotalPriceForItem(Item item)
-    {
-        var discounts = _shopRepository.GetDiscounts();
-        var totalPrice = discounts.Where(discount => discount.ItemId == item.Id)
-            .Aggregate(item.Price, (current, discount) => current - (current * discount.DiscountPercentage));
-        return totalPrice;
-    }
     
     public List<SubCategory> GetSubCategories()
     {
@@ -39,5 +32,64 @@ public class ShopService
         return _shopRepository.GetDiscounts();
     }
     
+    public List<ActiveItem> GetActiveItems()
+    {
+        return _shopRepository.GetActiveItems();
+    }
+    
+    public List<ConsumableItem> GetConsumableItems()
+    {
+        return _shopRepository.GetConsumableItems();
+    }
+    
+    public List<UserItem> GetUserItems()
+    {
+        return _shopRepository.GetUserItems();
+    }
+    
+    public List<ItemType> GetItemTypes()
+    {
+        return _shopRepository.GetItemTypes();
+    }
+
+    public int CalculateTotalPriceForItem(Item item)
+    {
+        var discounts = _shopRepository.GetDiscounts();
+        var totalPrice = discounts.Where(discount => discount.ItemId == item.Id)
+            .Aggregate(item.Price, (current, discount) => current - (current * discount.DiscountPercentage));
+        return totalPrice;
+    }
+    public ActiveItem? GetActiveItemByIds(int richbetUserId, int itemId)
+    {
+        return _shopRepository.GetActiveItemByIds(richbetUserId, itemId);
+    }
+    public Category? GetCategoryById(int categoryId)
+    {
+        return _shopRepository.GetCategoryById(categoryId);
+    }
+    public SubCategory? GetSubCategoryById(int subCategoryId)
+    {
+        return _shopRepository.GetSubCategoryById(subCategoryId);
+    }
+    public ConsumableItem? GetConsumableItemByItemId(int itemId)
+    {
+        return _shopRepository.GetConsumableItemByItemId(itemId);
+    }
+    public Discount? GetDiscountByItemId(int itemId)
+    {
+        return _shopRepository.GetDiscountByItemId(itemId);
+    }
+    public Item? GetItemById(int itemId)
+    {
+        return _shopRepository.GetItemById(itemId);
+    }
+    public UserItem? GetUserItemByIds(int richbetUserId, int itemId)
+    {
+        return _shopRepository.GetUserItemByIds(richbetUserId, itemId);
+    }
+    public ItemType? GetItemTypeByItemId(int itemId)
+    {
+        return _shopRepository.GetItemTypeByItemId(itemId);
+    }
 
 }
