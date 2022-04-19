faa991f4927eb50fa35bd2490c46af5dee117134
Drairan9 <drairan@protonmail.com>
2022-04-19 19:32:37 +0200

    pt4

---
+++ RichbetsResurrected/src/RichbetsResurrected.Web/ViewModels/CustomStylingViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RichbetsResurrected.Web.ViewModels;
+
+public class CustomStylingViewModel
+{
+    public List<SubCategory> SubCategories { get; set; } 
+}
