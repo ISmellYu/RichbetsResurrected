c3ac1508cd84532d8f0998e9054638698e928f26
ISmellYu <lukaszszostak10@gmail.com>
2022-04-19 23:05:26 +0200

    huj

--- RichbetsResurrected/src/RichbetsResurrected.Web/Controllers/GamesController.cs
+++ RichbetsResurrected/src/RichbetsResurrected.Web/Controllers/GamesController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RichbetsResurrected.Identity.Repositories;
+using RichbetsResurrected.Interfaces.Shop;
 using RichbetsResurrected.Services.Shop;
 
 namespace RichbetsResurrected.Web.Controllers;
@@ -8,7 +9,7 @@ namespace RichbetsResurrected.Web.Controllers;
 [ApiExplorerSettings(IgnoreApi = true)]
 public class GamesController : Controller
 {
-    private readonly ShopService _shopService;
+    private readonly IShopService _shopService;
     public GamesController(ShopService shopService)
     {
         _shopService = shopService;
