98e72e1e8d515b73d4fc97ee1c84ebb3044d3df9
ISmellYu <lukaszszostak10@gmail.com>
2022-06-02 21:22:34 +0200

    Opened static files to Unity

--- RichbetsResurrected/src/RichbetsResurrected.Utilities/Constants/OAuthConstants.cs
+++ RichbetsResurrected/src/RichbetsResurrected.Utilities/Constants/OAuthConstants.cs
@@ -1,6 +1,6 @@
 ﻿namespace RichbetsResurrected.Utilities.Constants;
 
-public class OAuthConstants
+public static class OAuthConstants
 {
     public const string DiscordId = "urn:discord:user:id";
     public const string AvatarUrl = "urn:discord:user:avatar";
