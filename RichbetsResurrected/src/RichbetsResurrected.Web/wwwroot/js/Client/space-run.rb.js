a0aec3ec68f96d0d3c22c90cf1fa52d0b85c2f59
Drairan9 <drairan@protonmail.com>
2022-06-28 22:57:26 +0200

    Fixed reset detection on spacerun

--- RichbetsResurrected/src/RichbetsResurrected.Web/wwwroot/js/Client/space-run.rb.js
+++ RichbetsResurrected/src/RichbetsResurrected.Web/wwwroot/js/Client/space-run.rb.js
@@ -2,15 +2,17 @@ let conn = new signalR.HubConnectionBuilder().withUrl("/crashHub").build();
 
 let oldData;
 let oldCrashed;
-let oldMulti;
 let oldStart;
+let oldTime;
+let _checkAppStart = true;
+
 conn.start().then(function () {
     conn.stream("StreamCrashInfo").subscribe({
         next: function (data) {
-          console.log(data);
+
           if (wakeup == true) {
             document.getElementById("space-multiplier").textContent = data.multiplier + "X";
-            
+
             if (oldData != data) {
               let object = constructObj(data);
               sendDataToWebGL(JSON.stringify(object));
@@ -19,22 +21,31 @@ conn.start().then(function () {
 
             if (oldCrashed != data.crashed) {
               if (data.crashed == true) {
+                console.log("Crashed");
                 unityInstance.SendMessage('Main Camera', 'CrashRocket')
                 document.getElementById("space-multiplier").style.color = "#EE5353";
               }
               oldCrashed = data.crashed;
             }
 
-            if (oldMulti != data.multiplier) {
-              if (!data.allowRemovingBets && data.timeLeft > 0) {
+            if (data.timeLeft == 10) {
+              console.log("reset rocket")
+              unityInstance.SendMessage('Main Camera', 'ResetRocket')
+              document.getElementById("space-multiplier").style.color = "white";
+            }
+
+            if (data.timeLeft > 0) {
+              document.getElementById("space-multiplier").textContent = data.timeLeft;
+
+              if (_checkAppStart) {
                 unityInstance.SendMessage('Main Camera', 'ResetRocket')
-                document.getElementById("space-multiplier").style.color = "white";
+                _checkAppStart = false;
               }
-              oldMulti = data.multiplier;
             }
 
             if (oldStart != data.allowPlacingBets) {
               if (data.timeLeft == 0) {
+                console.log("Start rocket");
                 unityInstance.SendMessage('Main Camera', 'StartRocket')
               }
               oldStart = data.allowPlacingBets;
