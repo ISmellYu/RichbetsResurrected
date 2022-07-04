efeed5f4d9d88d5855e254686a2381afbdd9fbf4
Drairan9 <drairan@protonmail.com>
2022-07-04 05:11:57 +0200

    Spacerun ready for testing

--- RichbetsResurrected/src/RichbetsResurrected.Web/wwwroot/js/Games/Spacerun/spacerun-controller.rb.js
+++ RichbetsResurrected/src/RichbetsResurrected.Web/wwwroot/js/Games/Spacerun/spacerun-controller.rb.js
@@ -1,95 +1,201 @@
 let crashConn = new signalR.HubConnectionBuilder().withUrl("/crashHub").build();
 
 crashConn.start().then(function () {
+    var streamData;
+    var playerInit = true;
+    var oldCrashed;
+    var oldAllowPlacingBets;
+    var _total;
+    var playersOld;
 
-    var _crashStatic = {
-        playersOld: null,       // 
-        crashedOld: null,       // Old variables to compare if the players list changed
-        startOld: null,         //   
-        multiplierOld: null,    //
-    
-        allowPlacingBets: null,
-        crashed: null,
-        gameStarted: null
-    }
+    var playerData = {
+        isBetting: false,
+        amount: 0,
+        cashouted: false,
+        whenCashouted: 0,
+        autoCashout: false,
+        whenAutoCashout: 0,
+        autoBetting: false,
+        autoBetsTotal: 0
+    };
 
-    var userData = {
-        betting: false,
-        betAmount: 0,
-        betMode: "manual",
-        whenCashouted: false
-    }
+    var manualButton = document.getElementById("manual-place-bet");
+    var autoButton = document.getElementById("auto-place-bet");
 
-    getUserData().then(function (data) {
-        let _playerData;
-        if (isPlayerBetting(data, _crashStatic.playersOld)) {
-            userData.betting = true;
-            _playerData = isPlayerBetting(data, _crashStatic.playersOld);
-            userData.betAmount = _playerData.amount;
-            userData.whenCashouted = _playerData.whenCashouted;
+    $("#manual-place-bet").click(function () {
+        placeBet(1000);
+        if (playerData.isBetting) {
+            cashout();
+        }
+    });
+
+    $("#auto-place-bet").click(function () {
+        if (playerData.autoBetting) {
+            playerData.autoBetting = false;
+            autoButton.textContent = "Start Autobet"
+        }else{
+            playerData.autoBetting = true;
+            autoButton.textContent = "Autobet active"
+            placeBet(100);
         }
     });
 
     crashConn.stream("StreamCrashInfo").subscribe({
         next: function (data) {
-            _crashStatic.gameStarted = data.allowRemovingBets;
+            streamData = data;
 
-            // console.log(userData.betAmount);
-            // console.log(userData.betting);
-            //console.log(userData.whenCashouted);
+            // Render dev data
+            document.getElementById("dev-info").innerHTML = "";
+            (Object.entries(playerData)).forEach(element => {
+                let para = document.createElement("p");
+                para.innerHTML = element[0] + ": " + element[1];
+                document.getElementById("dev-info").appendChild(para);
+            });
 
-            if (JSON.stringify(data.players) !== JSON.stringify(_crashStatic.playersOld)) { // If the players list changed
-                _crashStatic.playersOld = data.players;
+            if (JSON.stringify(data.players) !== JSON.stringify(playersOld)) { // If the players list changed
+                playersOld = data.players;
 
                 renderPlayersList(data.players);
             }
 
-            if (_crashStatic.crashedOld != data.crashed) {
+            if (oldCrashed != data.crashed) {
                 if (data.crashed == true) {
+                    clearPlayerData();
                     renderPlayersLostList(data.players);
                 }
-                _crashStatic.crashedOld = data.crashed;
+                oldCrashed = data.crashed;
             }
 
-            if (_crashStatic.multiplierOld != data.multiplier) {
-                if (userData.betting) {
-                    if (userData.betAmount != 0) {
-                        if (userData.whenCashouted) {
-                            $("#manual-place-bet").text(`Cashouted ${Math.trunc(userData.betAmount * _crashStatic.multiplierOld)}`);
-                        }else{
-                            $("#manual-place-bet").text(
-                                `Cashout ${Math.trunc(userData.betAmount * data.multiplier)}`
-                            );
-                        }
+            if (oldAllowPlacingBets != data.allowPlacingBets) {
+                if (data.allowPlacingBets == true) {
+                    if (playerData.autoBetting && playerData.autoBetsTotal > 0) {
+                        placeBet(100);
+                        document.getElementById("auto_total-bets").value -= 1;
+                    }
+                    if (playerData.autoBetsTotal == 0) {
+                        autoButton.textContent = "Start Autobet";
+                        playerData.autoBetting = false;
                     }
                 }
-                if (data.multiplier == 1) {
-                    // reset 
-                }
-                _crashStatic.multiplierOld = data.multiplier;
+                oldAllowPlacingBets = data.allowPlacingBets;
             }
-            if (data.multiplier == 1) {
-                if (userData.betAmount > 0) {
-                    $("#manual-place-bet").text(`Place bet (${userData.betAmount})`);
+            
+            if (playerInit == true) {
+                updatePlayerData();
+                playerInit = false;
+            }
+
+            let manual_cashout = document.getElementById("manual_cashout").value;
+            let auto_cashout = document.getElementById("auto_cashout").value;
+
+            if (manual_cashout) {
+                playerData.autoCashout = true;
+                playerData.whenAutoCashout = manual_cashout
+            }
+
+            if (auto_cashout) {
+                playerData.autoCashout = true;
+                playerData.whenAutoCashout = auto_cashout
+            }
+
+            if (!manual_cashout && !auto_cashout) {
+                playerData.autoCashout = false;
+                playerData.whenAutoCashout = 0;
+            }
+
+            if (playerData.isBetting && playerData.autoCashout) {
+                if (data.multiplier >= playerData.whenAutoCashout) {
+                    cashout();
+                }
+            }
+
+            if (playerData.autoBetting) {
+                playerData.autoBetsTotal = document.getElementById("auto_total-bets").value;
+            }
+
+            if (playerData.isBetting) {
+                if (streamData.gameStarted) {
+                    manualButton.textContent = "Cashout +" + (playerData.amount * streamData.multiplier).toFixed(0);
                 }else{
-                    $("#manual-place-bet").text("Place Bet");
+                    manualButton.textContent = "Place Bet (" + playerData.amount  +")";
+                }
+            }else{
+                if (playerData.whenCashouted) {
+                    manualButton.textContent = "Cashouted";
+                }else{
+                    manualButton.textContent = "Place Bet";
+                }
+                if (streamData.gameStarted) {
+                    lockBettingButtons(true);
+                }else{
+                    lockBettingButtons(false);
                 }
             }
         }
     });
 
-    // Client control section
-
-    $("#manual-place-bet").click(function () {
-        if (_crashStatic.gameStarted == true) {
-            cashout();
+    async function placeBet(amount) {
+        let result = await crashConn.invoke("JoinCrash", amount).catch(function (err) {
+            return console.error(err.toString());
+        });
+        if (result.isSuccess == true) {
+            playerData.isBetting = true;
+            playerData.amount += amount;
         }
-        else{
-            placeBet(1000);
-        }
-    });
+    }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    async function cashout() {
+        let result = await crashConn.invoke("Cashout").catch(function (err) {
+            return console.error(err.toString());
+        });
+        if (result.isSuccess == true) {
+            updatePlayerData();
+        }
+    }
+
+    async function getUserData() {
+
+        let result = await connection.invoke("GetClientInfo").catch(function (err) {
+            return console.error(err.toString());
+    
+        });
+        return result;
+    }
+
+    function lockBettingButtons(status){
+        if (status == true) {
+            document.getElementById("manual-place-bet").classList.add("bet-button-disabled"); 
+            document.getElementById("auto-place-bet").classList.add("bet-button-disabled");
+        }else{
+            document.getElementById("manual-place-bet").classList.remove("bet-button-disabled"); 
+            document.getElementById("auto-place-bet").classList.remove("bet-button-disabled");
+        }
+    }
+
+    function updatePlayerData() {
+        getUserData().then(function (data) {
+            (streamData.players).forEach(element => {
+                if (element.discordUserId == data.discordUserId) {
+                    if (element.cashouted) {
+                        playerData.isBetting = false;
+                        playerData.amount = 0
+                    }else{
+                        playerData.isBetting = true;
+                        playerData.amount = element.amount;
+                    }
+                    playerData.cashouted = element.cashouted;
+                    playerData.whenCashouted = element.whenCashouted;
+                }
+            });
+        });
+    }
+
+    function clearPlayerData() {
+        playerData.cashouted = false;
+        playerData.whenCashouted = 0;
+        playerData.amount = 0;
+        playerData.isBetting = false;
+    }
 
     function renderPlayersList(data) {
         let _total = 0;
@@ -147,45 +253,5 @@ crashConn.start().then(function () {
 
     function clearPlayersList() {
         $(".players-table").empty();
-    }
-
-    async function placeBet(amount) {
-        let result = await crashConn.invoke("JoinCrash", amount).catch(function (err) {
-            return console.error(err.toString());
-        });
-        if (result.isSuccess == true) {
-            userData.betting = true;
-            userData.betAmount += parseInt(amount);
-        }
-    }
-
-    async function cashout() {
-        let result = await crashConn.invoke("Cashout").catch(function (err) {
-            return console.error(err.toString());
-        });
-        if (result.isSuccess == true) {
-            $("#manual-place-bet").text(`Cashouted ${Math.trunc(userData.betAmount * _crashStatic.multiplierOld)}`);
-            userData.betting = false;
-            userData.betAmount = 0;
-            userData.whenCashouted = _crashStatic.multiplierOld;
-        }
-    }
-
-    async function getUserData() {
-        let result = await connection.invoke("GetClientInfo").catch(function (err) {
-    
-            return console.error(err.toString());
-    
-        });
-        return result;
-    }
-
-    function isPlayerBetting(playerData, gamePlayersData) {
-        for (let i = 0; i < gamePlayersData.length; i++) {
-            if (playerData.discordUserId == gamePlayersData[i].discordUserId) {
-                return gamePlayersData[i];
-            }
-        }
-        return false;
     }
 });
