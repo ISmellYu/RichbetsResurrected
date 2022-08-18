fa4a0e8adbbff71ab5d74b6f2ef11825278e564e
ISmellYu <lukaszszostak10@gmail.com>
2022-08-18 16:53:44 +0200

    Added statistics

---
+++ RichbetsResurrected/src/RichbetsResurrected.Identity/Repositories/StatisticsRepository.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RichbetsResurrected.Entities.DatabaseEntities.Statistics;
+using RichbetsResurrected.Identity.Contexts;
+using RichbetsResurrected.Interfaces.DAL.Statistics;
+
+namespace RichbetsResurrected.Identity.Repositories;
+
+public class StatisticsRepository : IStatisticsRepository
+{
+    private readonly AppDbContext _context;
+
+    public DbSet<Statistic> Statistics => _context.Statistics;
+
+    public StatisticsRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Statistic> GetStatisticAsync(int userId)
+    {
+        if (!await DoesStatisticsExistAsync(userId))
+            await AddStatisticAsync(userId);
+        var stat = await Statistics.AsNoTracking().FirstAsync(p => p.RichetUserId == userId);
+        return stat;
+    }
+    public async Task AddStatisticAsync(int userId)
+    {
+        if (await DoesStatisticsExistAsync(userId))
+            return;
+        var stat = new Statistic()
+        {
+            LoseAmount = 0, WinAmount = 0, RichetUserId = userId
+        };
+        
+        await _context.Statistics.AddAsync(stat);
+        await _context.SaveChangesAsync();
+    }
+    
+    public async Task<bool> DoesStatisticsExistAsync(int userId)
+    {
+        return await Statistics.FindAsync(userId) != null;
+    }
+    
+    public async Task AddToWonPointsAsync(int userId, int points)
+    {
+        if (!await DoesStatisticsExistAsync(userId))
+        {
+            await AddStatisticAsync(userId);
+        }
+        
+        var stat = await Statistics.FindAsync(userId);
+        if (stat == null)
+            return;
+        stat.WinAmount += points;
+        await _context.SaveChangesAsync();
+    }
+    public async Task AddToLostPointsAsync(int userId, int points)
+    {
+        if (!await DoesStatisticsExistAsync(userId))
+        {
+            await AddStatisticAsync(userId);
+        }
+        
+        var stat = await Statistics.FindAsync(userId);
+        if (stat == null)
+            return;
+        stat.LoseAmount += points;
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ RichbetsResurrected/src/RichbetsResurrected.Interfaces/DAL/Statistics/IStatisticsRepository.cs
@@ -0,0 +1,12 @@
+﻿using RichbetsResurrected.Entities.DatabaseEntities.Statistics;
+
+namespace RichbetsResurrected.Interfaces.DAL.Statistics;
+
+public interface IStatisticsRepository
+{
+    Task<Statistic> GetStatisticAsync(int userId);
+    Task AddStatisticAsync(int userId);
+    Task<bool> DoesStatisticsExistAsync(int userId);
+    Task AddToWonPointsAsync(int userId, int points);
+    Task AddToLostPointsAsync(int userId, int points);
+}
