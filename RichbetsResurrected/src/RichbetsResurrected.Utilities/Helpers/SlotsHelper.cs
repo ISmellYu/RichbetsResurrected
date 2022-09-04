8f6e3efd8bbb8dd4fdf6e10a6e43b4c4ffab0636
ISmellYu <lukaszszostak10@gmail.com>
2022-09-04 03:12:51 +0200

    Changed multipliers

--- RichbetsResurrected/src/RichbetsResurrected.Utilities/Helpers/SlotsHelper.cs
+++ RichbetsResurrected/src/RichbetsResurrected.Utilities/Helpers/SlotsHelper.cs
@@ -8,56 +8,56 @@ public static class SlotsHelper
     {
         {
             SymbolEnum.Seven, new Dictionary<int, float>(){
-                {2, 40f},
-                {3, 100f},
+                {2, 20f},
+                {3, 50f},
             }
         },
         {
             SymbolEnum.Watermelon, new Dictionary<int, float>(){
-                {2, 4f},
-                {3, 10f},
-            }
-        },
-        {
-            SymbolEnum.Grape, new Dictionary<int, float>(){
-                {2, 3f},
-                {3, 6f},
-            }
-        },
-        {
-            SymbolEnum.Lemon, new Dictionary<int, float>(){
-                {2, 2f},
-                {3, 3.5f},
-            }
-        },
-        {
-            SymbolEnum.Cherry, new Dictionary<int, float>(){
-                {2, 1.5f},
-                {3, 3f},
-            }
-        },
-        {
-            SymbolEnum.Plum, new Dictionary<int, float>(){
-                {2, 2f},
-                {3, 3.5f},
-            }
-        },
-        {
-            SymbolEnum.Orange, new Dictionary<int, float>(){
                 {2, 2.5f},
                 {3, 5f},
             }
         },
         {
+            SymbolEnum.Grape, new Dictionary<int, float>(){
+                {2, 1.5f},
+                {3, 3f},
+            }
+        },
+        {
+            SymbolEnum.Lemon, new Dictionary<int, float>(){
+                {2, 1f},
+                {3, 1.5f},
+            }
+        },
+        {
+            SymbolEnum.Cherry, new Dictionary<int, float>(){
+                {2, 1f},
+                {3, 1.5f},
+            }
+        },
+        {
+            SymbolEnum.Plum, new Dictionary<int, float>(){
+                {2, 1f},
+                {3, 1.5f},
+            }
+        },
+        {
+            SymbolEnum.Orange, new Dictionary<int, float>(){
+                {2, 1f},
+                {3, 5f},
+            }
+        },
+        {
             SymbolEnum.Diamond, new Dictionary<int, float>(){
-                {2, 20f},
-                {3, 60f},
+                {2, 10f},
+                {3, 30f},
             }
         },
         {
             SymbolEnum.Shooter, new Dictionary<int, float>(){
-                {2, 80f},
-                {3, 350f},
+                {2, 40f},
+                {3, 175f},
             }
         }
     };
