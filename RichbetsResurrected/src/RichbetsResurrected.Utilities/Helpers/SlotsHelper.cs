164e3b14ee4c5ff3ef81035da59408160afe558c
ISmellYu <lukaszszostak10@gmail.com>
2022-09-04 03:48:29 +0200

    Adjusted multipliers

--- RichbetsResurrected/src/RichbetsResurrected.Utilities/Helpers/SlotsHelper.cs
+++ RichbetsResurrected/src/RichbetsResurrected.Utilities/Helpers/SlotsHelper.cs
@@ -8,56 +8,56 @@ public static class SlotsHelper
     {
         {
             SymbolEnum.Seven, new Dictionary<int, float>(){
-                {2, 20f},
-                {3, 50f},
+                {2, 7.5f},
+                {3, 20f},
             }
         },
         {
             SymbolEnum.Watermelon, new Dictionary<int, float>(){
-                {2, 2.5f},
-                {3, 5f},
+                {2, 1.75f},
+                {3, 2.5f},
             }
         },
         {
             SymbolEnum.Grape, new Dictionary<int, float>(){
-                {2, 1.5f},
-                {3, 3f},
+                {2, 0.5f},
+                {3, 1.5f},
             }
         },
         {
             SymbolEnum.Lemon, new Dictionary<int, float>(){
-                {2, 1f},
+                {2, 0.5f},
                 {3, 1.5f},
             }
         },
         {
             SymbolEnum.Cherry, new Dictionary<int, float>(){
-                {2, 1f},
+                {2, 0.5f},
                 {3, 1.5f},
             }
         },
         {
             SymbolEnum.Plum, new Dictionary<int, float>(){
-                {2, 1f},
+                {2, 0.5f},
                 {3, 1.5f},
             }
         },
         {
             SymbolEnum.Orange, new Dictionary<int, float>(){
                 {2, 1f},
-                {3, 5f},
+                {3, 3f},
             }
         },
         {
             SymbolEnum.Diamond, new Dictionary<int, float>(){
-                {2, 10f},
-                {3, 30f},
+                {2, 7.5f},
+                {3, 25f},
             }
         },
         {
             SymbolEnum.Shooter, new Dictionary<int, float>(){
-                {2, 40f},
-                {3, 175f},
+                {2, 25f},
+                {3, 100f},
             }
         }
     };
